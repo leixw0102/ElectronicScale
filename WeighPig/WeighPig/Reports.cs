e52a2580d51d35abccadd5f5cff6c8e058e1d35d
shenchen-PC\shenchen <331645431@qq.com>
2018-12-24 08:05:59 +0800

    create

---
+++ WeighPig/WeighPig/Reports.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeighPig
+{
+    class Reports
+    {
+        public string report_level { get; set; }
+        public string report_count { get; set; }
+        public string report_weight { get; set; }
+    }
+}
---
+++ WeighPig/WeighPig/Weights.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeighPig
+{
+    class Weights
+    {
+        public int id { get; set; }
+        public int sn { get; set; }
+        public string create_time { get; set; }
+        public string weight { get; set; }
+        public string level { get; set; }
+        public string remarks { get; set; }
+        public string type { get; set; }
+        public int is_upload { get; set; }
+        public int life_cycle { get; set; }
+    }
+}
