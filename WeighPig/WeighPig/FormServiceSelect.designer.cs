41fcf1e02cd5a3e8cb7a27b0c2a4035df87d4847
shenchen-PC\shenchen <331645431@qq.com>
2018-12-24 08:13:27 +0800

    create

---
+++ WeighPig/WeighPig/FormServiceSelect.designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WeighPig
+{
+    partial class FormServiceSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_date = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.grid_weights = new System.Windows.Forms.DataGridView();
+            this.grid_reports = new System.Windows.Forms.DataGridView();
+            this.report_level = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.report_count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.report_weight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.create_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.weight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.level = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.remarks = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.is_upload = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.life_cycle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_weights)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_reports)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // input_date
+            // 
+            this.input_date.Location = new System.Drawing.Point(60, 7);
+            this.input_date.Name = "input_date";
+            this.input_date.Size = new System.Drawing.Size(197, 21);
+            this.input_date.TabIndex = 16;
+            this.input_date.ValueChanged += new System.EventHandler(this.input_date_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "时间";
+            // 
+            // grid_weights
+            // 
+            this.grid_weights.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_weights.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.sn,
+            this.create_time,
+            this.weight,
+            this.level,
+            this.type,
+            this.remarks,
+            this.is_upload,
+            this.life_cycle});
+            this.grid_weights.Location = new System.Drawing.Point(12, 55);
+            this.grid_weights.Name = "grid_weights";
+            this.grid_weights.RowHeadersVisible = false;
+            this.grid_weights.RowTemplate.Height = 23;
+            this.grid_weights.Size = new System.Drawing.Size(576, 441);
+            this.grid_weights.TabIndex = 21;
+            // 
+            // grid_reports
+            // 
+            this.grid_reports.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_reports.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.report_level,
+            this.report_count,
+            this.report_weight});
+            this.grid_reports.Location = new System.Drawing.Point(608, 55);
+            this.grid_reports.Name = "grid_reports";
+            this.grid_reports.RowHeadersVisible = false;
+            this.grid_reports.RowTemplate.Height = 23;
+            this.grid_reports.Size = new System.Drawing.Size(261, 441);
+            this.grid_reports.TabIndex = 22;
+            // 
+            // report_level
+            // 
+            this.report_level.DataPropertyName = "report_level";
+            this.report_level.HeaderText = "标签";
+            this.report_level.Name = "report_level";
+            this.report_level.Width = 55;
+            // 
+            // report_count
+            // 
+            this.report_count.DataPropertyName = "report_count";
+            this.report_count.HeaderText = "累计数量";
+            this.report_count.Name = "report_count";
+            // 
+            // report_weight
+            // 
+            this.report_weight.DataPropertyName = "report_weight";
+            this.report_weight.HeaderText = "累计重量";
+            this.report_weight.Name = "report_weight";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(605, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 16);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "统计";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "明细";
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "Id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 40;
+            // 
+            // sn
+            // 
+            this.sn.DataPropertyName = "sn";
+            this.sn.HeaderText = "顺序号";
+            this.sn.Name = "sn";
+            this.sn.ReadOnly = true;
+            this.sn.Width = 70;
+            // 
+            // create_time
+            // 
+            this.create_time.DataPropertyName = "create_time";
+            this.create_time.HeaderText = "时间";
+            this.create_time.Name = "create_time";
+            this.create_time.ReadOnly = true;
+            this.create_time.Width = 120;
+            // 
+            // weight
+            // 
+            this.weight.DataPropertyName = "weight";
+            this.weight.HeaderText = "重量";
+            this.weight.Name = "weight";
+            this.weight.ReadOnly = true;
+            this.weight.Width = 60;
+            // 
+            // level
+            // 
+            this.level.DataPropertyName = "level";
+            this.level.HeaderText = "标签";
+            this.level.Name = "level";
+            this.level.ReadOnly = true;
+            this.level.Width = 60;
+            // 
+            // type
+            // 
+            this.type.DataPropertyName = "type";
+            this.type.HeaderText = "工艺";
+            this.type.Name = "type";
+            this.type.ReadOnly = true;
+            this.type.Width = 60;
+            // 
+            // remarks
+            // 
+            this.remarks.DataPropertyName = "remarks";
+            this.remarks.HeaderText = "备注";
+            this.remarks.Name = "remarks";
+            this.remarks.ReadOnly = true;
+            // 
+            // is_upload
+            // 
+            this.is_upload.DataPropertyName = "is_upload";
+            this.is_upload.HeaderText = "上传";
+            this.is_upload.Name = "is_upload";
+            this.is_upload.ReadOnly = true;
+            this.is_upload.Width = 60;
+            // 
+            // life_cycle
+            // 
+            this.life_cycle.DataPropertyName = "life_cycle";
+            this.life_cycle.HeaderText = "生命周期";
+            this.life_cycle.Name = "life_cycle";
+            this.life_cycle.Visible = false;
+            // 
+            // FormServiceSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 502);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.grid_reports);
+            this.Controls.Add(this.grid_weights);
+            this.Controls.Add(this.input_date);
+            this.Controls.Add(this.label4);
+            this.Name = "FormServiceSelect";
+            this.Text = "查询数据";
+            ((System.ComponentModel.ISupportInitialize)(this.grid_weights)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_reports)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker input_date;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView grid_weights;
+        private System.Windows.Forms.DataGridView grid_reports;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn report_level;
+        private System.Windows.Forms.DataGridViewTextBoxColumn report_count;
+        private System.Windows.Forms.DataGridViewTextBoxColumn report_weight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn create_time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn weight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn level;
+        private System.Windows.Forms.DataGridViewTextBoxColumn type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn remarks;
+        private System.Windows.Forms.DataGridViewTextBoxColumn is_upload;
+        private System.Windows.Forms.DataGridViewTextBoxColumn life_cycle;
+    }
+}
